d158556e21b7ce17e9c910cf783312af6144f022
yama42747 <yama42747@gmail.com>
2025-10-22 01:50:22 +0900

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,19 +1,6 @@
-export const onRequest = async ({ request, env }) => {
-  const auth = request.headers.get("Authorization");
-  const username = env.CF_PAGES_USERNAME;
-  const password = env.CF_PAGES_PASSWORD;
-  const realm = "Protected";
 
-  if (auth && auth.startsWith("Basic ")) {
-    const decoded = atob(auth.split(" ")[1]);
-    const [user, pass] = decoded.split(":");
-    if (user === username && pass === password) {
-      return; // 認証OK
-    }
-  }
 
-  return new Response("Unauthorized", {
-    status: 401,
-    headers: { "WWW-Authenticate": `Basic realm="${realm}"` },
-  });
+export const onRequest = async () => {
+  return new Response("ok");
 };
+
